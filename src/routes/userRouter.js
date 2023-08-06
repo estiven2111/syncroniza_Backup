226553463410700465192ab0cb2c1bac20355065
estiven2111 <estiven.a.martinez@gmail.com>
2023-08-05 21:14:52 -0500

    cls

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -166,7 +166,7 @@ passport.use(
     },
     (accessToken, refreshToken, params, profile, done) => {
       // console.log("token", accessToken);
-      console.log("params", params,"profile", profile,"done", done)
+     
       // aca puede realizar acciones para obtener los datos de los usuarios
       //para enviar ala base datos o lo que desee y se pueda hacer
 
