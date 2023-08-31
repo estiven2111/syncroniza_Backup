ab5f2a22a70eb918d8244317ac408f00badb831a
estiven2111 <estiven.a.martinez@gmail.com>
2023-08-31 07:50:54 -0500

    OCR3

--- src/middlewares/uploadFiles.js
+++ src/middlewares/uploadFiles.js
@@ -92,10 +92,10 @@ const dashboard = async (req, res) => {
   // //ActualizarProyecto
   //TODO creo un objeto con los valores que me llegan por el body
   
-  console.log("tipooo", tipo,req.files.imagen);
+  console.log("tipooo", tipo);
   let token = req.user.accessToken;
   let users;
-  switch (tipo) {
+  switch (tipo,req.files) {
     case "OCR":
       try {
         if (req.files) {
