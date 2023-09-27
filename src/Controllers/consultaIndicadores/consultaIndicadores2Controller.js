6716e6d88b2aa10224aa545fa958519367e5861a
estiven2111 <estiven.a.martinez@gmail.com>
2023-09-27 11:39:34 -0500

    horas

--- src/Controllers/consultaIndicadores/consultaIndicadores2Controller.js
+++ src/Controllers/consultaIndicadores/consultaIndicadores2Controller.js
@@ -2,8 +2,20 @@ const {sequelize} = require("../../db")
 const consultaIndicadores2Controller = async(req) =>{
     const {docId, id} = req.query
     console.log(docId, id)
+    let HorasProgramadaSinFrecuencia
+    let HorasCumplidasSinFrecuencia 
+
+    let HorasProgramadaConFrecuencia
+    let HorasCumplidasConFrecuencia
+
+    let HoraProgramada
+    let CumplidasPeriodo
+    let HorasFrecuencia
+    let atrazo
+    let HorasCumplidas
 try {
-    let CumplidadPeriodo1 = await sequelize.query(
+    //? consulta uno para sacar Horas programadas y el valor de cumplidas en el periodo
+    let Consulta1 = await sequelize.query(
         `
         select sum(RequeridoProyectoHH) HorasProgramadaSinFrecuencia, Sum(HorasRealProyecto) HorasCumplidasSinFrecuencia
 from TBL_SER_PROYECTOS A
@@ -15,15 +27,79 @@ and ((FechaInicio <= (select fin from TBL_CON_PERIODOCONTABLE where id= :id) AND
         `,
         {replacements:{ docId: docId, id:id}}
     )
-    if (CumplidadPeriodo1[0].length > 0) {
-        CumplidadPeriodo1 = CumplidadPeriodo1[0]
+
+       //? consulta dos para sacar Horas programadas y el valor de cumplidas en el periodo
+       let Consulta2 = await sequelize.query(
+        `
+        select sum(RequeridoProyectoHH) HorasProgramadaConFrecuencia, sum(RequeridoProyectoHH) HorasCumplidasConFrecuencia  --son las mismas pues nor equiere reporte
+        from TBL_SER_PROYECTOS A
+        inner join TBL_SER_ProyectoActividadesEmpleados b on a.idNodo=b.idNodoProyecto
+        inner join TBL_ESP_Procesos C on A.Cod_parte=C.ID
+        where (N_DocumentoEmpleado= :docId and C.AplicaFrecuencia=1)
+        and ((FechaInicio <= (select fin from TBL_CON_PERIODOCONTABLE where id=:id) AND FechaInicio >= (select Inicio from TBL_CON_PERIODOCONTABLE where id=:id))
+          or (FechaFinal <= (select fin from TBL_CON_PERIODOCONTABLE where id=:id) AND FechaFinal >= (select Inicio from TBL_CON_PERIODOCONTABLE where id=:id)))
+        `,
+        {replacements:{ docId: docId, id:id}}
+    )
+//todo ************************************************
+    //? validaciones consulta 1
+    if (Consulta1[0].length > 0 ) {
+        //! HorasProgramadaSinFrecuencia
+       if (Consulta1[0][0].HorasProgramadaSinFrecuencia) {
+        HorasProgramadaSinFrecuencia = Consulta1[0][0].HorasProgramadaSinFrecuencia
+       }else{
+        HorasProgramadaSinFrecuencia = 0
+       }
+       //! HorasCumplidasSinFrecuencia
+       if (Consulta1[0][0].HorasCumplidasSinFrecuencia) {
+
+        HorasCumplidasSinFrecuencia = Consulta1[0][0].HorasCumplidasSinFrecuencia
+       }else{
+        HorasCumplidasSinFrecuencia = 0
+       }
     }else{
-        CumplidadPeriodo1 = ""
+        HorasProgramadaSinFrecuencia = 0
+        HorasCumplidasSinFrecuencia = 0
     }
+    //todo ************************************************
+
+    //todo ************************************************
+    //? validaciones consulta 2
+
+    if (Consulta2[0].length > 0 ) {
+        //! HorasCumplidasConFrecuencia
+       if (Consulta1[0][0].HorasProgramadaConFrecuencia) {
+        HorasProgramadaConFrecuencia = Consulta1[0][0].HorasProgramadaConFrecuencia
+       }else{
+        HorasProgramadaConFrecuencia = 0
+       }
+       //! HorasCumplidasConFrecuencia
+       if (Consulta1[0][0].HorasCumplidasConFrecuencia) {
+
+        HorasCumplidasConFrecuencia = Consulta1[0][0].HorasCumplidasConFrecuencia
+       }else{
+        HorasCumplidasConFrecuencia = 0
+       }
+    }else{
+        HorasProgramadaConFrecuencia = 0
+        HorasCumplidasConFrecuencia = 0
+    }
+//todo ************************************************
+CumplidasPeriodo = HorasCumplidasSinFrecuencia + HorasCumplidasConFrecuencia
+HorasFrecuencia = HorasProgramadaConFrecuencia
+HoraProgramada = HorasProgramadaSinFrecuencia
+atrazo = (HorasProgramadaSinFrecuencia + HorasProgramadaConFrecuencia - CumplidasPeriodo)
+HorasCumplidas = CumplidasPeriodo + atrazo
+console.log("HorasCumplidasSinFrecuencia +  HorasCumplidasConFrecuencia = CumplidasPeriodo",CumplidasPeriodo )
+console.log("HorasProgramadaConFrecuencia", HorasFrecuencia)
+console.log("HorasProgramadaSinFrecuencia",HoraProgramada)
     const datos = {
-        HoraProgramada: CumplidadPeriodo1,
-        cumplidadperiodo : CumplidadPeriodo1,
-        Horafrecuencia:""
+        HoraProgramada,
+        HorasCumplidas,
+        CumplidasPeriodo,
+        atrazo,
+        HorasFrecuencia,
+        
     }
     return datos
 } catch (error) {
