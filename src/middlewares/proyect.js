b8952acdad71787386bef465ebcaad6635a80e44
estiven2111 <estiven.a.martinez@gmail.com>
2023-12-18 17:20:30 -0500

    hours

--- src/middlewares/proyect.js
+++ src/middlewares/proyect.js
@@ -330,29 +330,16 @@ const registerActivities = async (req, res) => {
 
 //todo devuelve la sumatoria de las actividades
 const hourActivities = async (req, res) => {
- 
   try {
-    // const consulta = await sequelize.query(`select * from Tbl_USUARIOS`);
-    // res.json(consulta);
-    res.json("hola")
+    const { idNodoProyecto, idNodoActividad } = req.query;
+    const hours = await sequelize.query(
+      `SELECT SUM(DuracionHoras) as horas FROM TBL_SER_ReporteHorasActividadEmpleado where idNodoProyecto = ${idNodoProyecto} AND idNodoActividad = ${idNodoActividad}`
+    );
+    const TotalH = parseFloat(hours[0][0].horas).toFixed(2);
+    res.json(TotalH);
   } catch (error) {
-    console.error("Error de Sequelize:", error);
-    res.status(500).json({ error: "Error al ejecutar la consulta en la base de datos." });
+    res.json({"error": error});
   }
-  
-
-
-
-  // try {
-  //   const { idNodoProyecto, idNodoActividad } = req.query;
-  //   const hours = await sequelize.query(
-  //     `SELECT SUM(DuracionHoras) as horas FROM TBL_SER_ReporteHorasActividadEmpleado where idNodoProyecto = ${idNodoProyecto} AND idNodoActividad = ${idNodoActividad}`
-  //   );
-  //   const TotalH = parseFloat(hours[0][0].horas).toFixed(2);
-  //   res.json(TotalH);
-  // } catch (error) {
-  //   res.json({"error": error});
-  // }
 };
 
 const updateProyecto = async (req, res) => {
