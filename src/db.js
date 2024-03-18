6430cb15f48be5c6ba2df54c57b56f082e920646
estiven2111 <estiven.a.martinez@gmail.com>
2024-03-18 07:13:34 -0500

    ok conexion

--- src/db.js
+++ src/db.js
@@ -55,57 +55,57 @@ const handb = async (req, res) => {
 //   // port:"1433"
 // });
 
-// sequelize = new Sequelize(DB_NAME1, BD_USERNAME1, DB_PASSWORD1, {
-//   host: DB_HOST1,
-//   dialect:
-//     "mssql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
-//   // port:"1433"
-// });
+sequelize = new Sequelize(DB_NAME1, BD_USERNAME1, DB_PASSWORD1, {
+  host: DB_HOST1,
+  dialect:
+    "mssql" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+  // port:"1433"
+});
 
 
 
 
 //todo base datos de azure ********************************************
 
-sequelize = new Sequelize({
-  dialect: "mssql",
-  host: DB_HOST, //creame-sim\\SQLEXPRESS   creame-sim-sql.database.windows.net
-  port: 1433,
-  database: DB_NAME, //BDCREAME    creame-sim-db
-  username: DB_PASSWORD, // Reemplaza con tu nombre de usuario
-  password: BD_USERNAME, // Reemplaza con tu contraseña
-  dialectOptions: {
-    options: {
-      encrypt: false,
-      trustServerCertificate: false,
-      authentication: "ActiveDirectoryDefault",
-    },
-  },
-  define: {
-    timestamps: false, // Si no estás usando campos de timestamp en tus modelos
-  },
-  logging: console.log, // Muestra logs en la consola (opcional, para debug)
-});
+// sequelize = new Sequelize({
+//   dialect: "mssql",
+//   host: DB_HOST, //creame-sim\\SQLEXPRESS   creame-sim-sql.database.windows.net
+//   port: 1433,
+//   database: DB_NAME, //BDCREAME    creame-sim-db
+//   username: DB_PASSWORD, // Reemplaza con tu nombre de usuario
+//   password: BD_USERNAME, // Reemplaza con tu contraseña
+//   dialectOptions: {
+//     options: {
+//       encrypt: false,
+//       trustServerCertificate: false,
+//       authentication: "ActiveDirectoryDefault",
+//     },
+//   },
+//   define: {
+//     timestamps: false, // Si no estás usando campos de timestamp en tus modelos
+//   },
+//   logging: console.log, // Muestra logs en la consola (opcional, para debug)
+// });
 
-sequelize = new Sequelize({
-  dialect: "mssql",
-  host: DB_HOST, //creame-sim\\SQLEXPRESS   creame-sim-sql.database.windows.net
-  port: 1433,
-  database: DB_NAME, //BDCREAME    creame-sim-db
-  username: BD_USERNAME, // Reemplaza con tu nombre de usuario
-  password: DB_PASSWORD, // Reemplaza con tu contraseña
-  dialectOptions: {
-    options: {
-      encrypt: false,
-      trustServerCertificate: false,
-      authentication: "ActiveDirectoryDefault",
-    },
-  },
-  define: {
-    timestamps: false, // Si no estás usando campos de timestamp en tus modelos
-  },
-  logging: console.log, // Muestra logs en la consola (opcional, para debug)
-});
+// sequelize = new Sequelize({
+//   dialect: "mssql",
+//   host: DB_HOST, //creame-sim\\SQLEXPRESS   creame-sim-sql.database.windows.net
+//   port: 1433,
+//   database: DB_NAME, //BDCREAME    creame-sim-db
+//   username: BD_USERNAME, // Reemplaza con tu nombre de usuario
+//   password: DB_PASSWORD, // Reemplaza con tu contraseña
+//   dialectOptions: {
+//     options: {
+//       encrypt: false,
+//       trustServerCertificate: false,
+//       authentication: "ActiveDirectoryDefault",
+//     },
+//   },
+//   define: {
+//     timestamps: false, // Si no estás usando campos de timestamp en tus modelos
+//   },
+//   logging: console.log, // Muestra logs en la consola (opcional, para debug)
+// });
 
 //todo esto para conectar las dos solo si es una o otra
 
@@ -130,6 +130,7 @@ const reincioNode = () => {
   proceso.on("close", (codigo) => {
     console.log("nodemon reiniciado");
   });
+
 
   //todo este funciona revisar en el deploy
   //   const kill = process.platform === 'win32' ? 'taskkill /F /IM node' : 'pkill node';
