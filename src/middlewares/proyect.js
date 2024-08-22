5b6cb0add76f3aabaea31899319ef225a023af72
estiven2111 <estiven.a.martinez@gmail.com>
2024-08-22 17:34:33 -0500

    error solucion1

--- src/middlewares/proyect.js
+++ src/middlewares/proyect.js
@@ -53,7 +53,7 @@ order by SKU_Logistica desc
         //todo Validar que el proyecto este correcto 
 
        
-        const existe = validateproyect[0].some(SKU => SKU.SKU_Logistica === proyect[0][0].SKU)
+        // const existe = validateproyect[0].some(SKU => SKU.SKU_Logistica === proyect[0][0].SKU)
         
         if (proyect[0]) {
           if (proyect[0]) {
