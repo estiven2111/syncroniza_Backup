f172935917b19f8df8ab51bf883c31b67b44d8a3
estiven2111 <estiven.a.martinez@gmail.com>
2024-11-25 09:01:20 -0500

    OPENAI AZURE

--- src/middlewares/Ocr.js
+++ src/middlewares/Ocr.js
@@ -281,7 +281,7 @@ async function Ocr(req, res) {
                 - ValordelreteICA pero lo dejas como ica
                 - valorderetefuente pero lo dejas como rete
                 - numerofactura pero lo dejas como numFact
-                - Concepto
+                - Concepto aca el valor que me vas a poner siempre va ser o producto o servicio nada mas eso depende de la factura 
                 Texto: "${texto}"`,
             },
           ],
