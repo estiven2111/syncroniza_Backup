d860f7c9362dddf697344747dd9730d4161b23b1
estiven2111 <estiven.a.martinez@gmail.com>
2025-07-09 21:59:49 -0500

    urls

--- src/middlewares/Ocr.js
+++ src/middlewares/Ocr.js
@@ -1394,6 +1394,8 @@ que pasa pues agrega el campo detalles_compra al objeto JSON final:
 NOTA
 si algun dato no lo encuentras ponlo como "" no me devuelvas undefinido o null.
 Recuerda, eres un contador experto, extrae solo lo que esté en la factura y realiza los cálculos si es necesario, con precisión contable.
+no adivines valores si son recibos de caja verifica por lo general no traen subtotal si lo traen ponlo pero casi siempre solo es el total 
+pon los datos encontrados no calcules no asumas pon los datos que se te piden y ya 
 `;
     const messages = [
       {
